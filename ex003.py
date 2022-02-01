db98d47b9d5bc70ac534456c89adc7ae952d1beb
dorzao <arcados2@protonmail.com>
2022-01-31 22:38:50 -0300

    subindo os arquivos

---
+++ ex003.py
@@ -0,0 +1,5 @@
+#aplicando o novo método de print com o .format
+numero1 = int(input('Digite um valor: '))
+numero2 = int(input('Digite um segundo valor: '))
+soma = numero1 + numero2
+print('A soma entre {} e {} é igual à {}'.format(numero2, numero1, soma))
